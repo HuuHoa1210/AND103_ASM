075a2c2c2af98eb6c48feed6d8b69d7f2e99e2d4
ASM <hoaro57@gmail.com>
2025-12-09 10:29:04 +0700

    new-update

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
+// 1. GỌI DOTENV NGAY DÒNG ĐẦU TIÊN
+require('dotenv').config(); 
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
 var mongoose = require('mongoose');
+
+// Import Models
 require('./models/user');
 require('./models/category');
 require('./models/product');
 require('./models/order');
 require('./models/orderdetail');
-require('./models/cartItem'); // Lưu ý: file bạn gửi là cartItem.js
+require('./models/cartItem');
 require('./models/image');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-
+// Import Routes
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var categoryRouter = require('./routes/categoryRouter');
@@ -24,6 +25,7 @@ var productRouter = require('./routes/productRouter');
 var orderRouter = require('./routes/orderRouter');
 var userRouter1 = require('./routes/userRouter1');
 var cartRouter = require('./routes/cartRouter');
+
 var app = express();
 
 // view engine setup
@@ -36,10 +38,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-//connect database
+// --- 2. KẾT NỐI MONGODB DÙNG BIẾN MÔI TRƯỜNG ---
 const connectDB = async () => {
   try {
+    // Nó sẽ lấy chuỗi từ file .env, nếu không có thì chạy localhost (dự phòng)
     const uri = process.env.MONGO_URI || "mongodb://localhost:27017/assignment";
+    
     await mongoose.connect(uri);
     console.log('>>>>>>>>>> DB Connected!!!!!!');
   } catch (err) {
@@ -48,31 +52,32 @@ const connectDB = async () => {
 };
 connectDB();
 
+// Routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/category', categoryRouter);
-app.use('/product', productRouter); // Android gọi: /product/all
+app.use('/product', productRouter);
 app.use('/order', orderRouter);
 app.use('/user', userRouter1);
 app.use('/cart', cartRouter);
-// catch 404 and forward to error handler
+
+// catch 404
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
 
+// --- 3. LẮNG NGHE PORT ---
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server đang chạy ở port ${PORT}`);
 });
+
 module.exports = app;
